f5b5580ddc1c616facc7708ba5cd00da4b4284fe
AhmadJafar360 <ahmadjafarali360@gmail.com>
2024-12-25 23:14:36 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Button from './components/mainButton.vue'
+</script>
+
+<template>
+  <Button />
+  <h1>Hai Programmer Vue</h1>
+  <p>Ahmad Ja'far Ali</p>
+</template>
+
+<style scoped></style>
